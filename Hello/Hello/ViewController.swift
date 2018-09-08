9b176c1e68fcea7312e5f940e14f009e3636d54d
kons16 <ks1216r@gmail.com>
2018-09-08 20:07:41 +0900

    chapter6まで

---
+++ Hello/Hello/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by KonoShinji on 2018/09/08.
+//  Copyright © 2018年 Kono Shinji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    @IBAction func sayHello() {
+        label.text = "こんにちは"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
